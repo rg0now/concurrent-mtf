b845e5e8ce2a829e64d5bebd7deb434c3a223532
Gabor Retvari <retvari@tmit.bme.hu>
2024-04-02 14:54:53 +0200

    Init data structures in parallel

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-        "fmt"
-        "time"
-        "os"
-        "strings"
-        "sync"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"time"
 
-        flag "github.com/spf13/pflag"
+	flag "github.com/spf13/pflag"
 )
 
-
 const BusyWait = 50000
+
 // const BusyWait = 100000
 const BufferSize int = 1000
-//const BufferSize int = 1
-//const CacheSize float64 = 0.2
+
+// const BufferSize int = 1
+// const CacheSize float64 = 0.2
 // const CacheSize float64 = 0.025
 // const CacheSize float64 = 0.1
 const CacheSize float64 = 0.05
@@ -23,144 +24,184 @@ const CacheSize float64 = 0.05
 var verbose bool = false
 
 func log(format string, v ...any) {
-        if verbose {
-                fmt.Printf(format + "\n", v...)
-        }
+	if verbose {
+		fmt.Printf(format+"\n", v...)
+	}
 }
 
 type Id = int
 type Channel chan Item
 
 type Item interface {
-        Less(Item) bool
-        Id() Id
-        Match(i Id) bool
+	Less(Item) bool
+	Id() Id
+	Match(i Id) bool
 }
 
 type Source interface {
-        Generate() (Item, error)
+	Generate() (Item, error)
 }
 
 type LoadBalancer interface {
-        Assign(i Item)
+	Assign(i Item)
 }
 
 type DataStructure interface {
-        Add(p Item)
-        Find(i Item) int
-        String() string
+	Add(p Item)
+	Find(i Item) int
+	String() string
 }
 
 type Thread struct {
-        id int
-        rx Channel
-        d DataStructure
+	id int
+	rx Channel
+	d  DataStructure
 }
 
+var ThreadStore []*Thread
+
 func main() {
-        // general flags
-        os.Args[0] = "cmtf"
-        var n   = flag.IntP("req_num", "n", 10, "Number of requests.")
-        var m   = flag.IntP("item_num", "m", 5, "Number of items.")
-        var k   = flag.IntP("thread_num", "k", 1, "Number of threads.")
-        var ds  = flag.StringP("data-structure", "d", "mtf", "Data-structure: cache|statcache|mtf|linkedlist|splay|btree (default: mtf).")
-        var src = flag.StringP("source", "s", "uniform", "Source: uniform|poisson (default: uniform).")
-        var sp  = flag.StringP("load-balancer", "l", "modulo", "Load-balaner: modulo|split|roundrobin (default: modulo).")
-        var v   = flag.BoolP("verbose", "v", false, "Verbose logging, identical to <-l all:DEBUG>.")
-        flag.Parse()
-        verbose = *v
+	// general flags
+	os.Args[0] = "cmtf"
+	var n = flag.IntP("req_num", "n", 10, "Number of requests.")
+	var m = flag.IntP("item_num", "m", 5, "Number of items.")
+	var k = flag.IntP("thread_num", "k", 1, "Number of threads.")
+	var ds = flag.StringP("data-structure", "d", "mtf", "Data-structure: cache|statcache|mtf|linkedlist|splay|btree (default: mtf).")
+	var src = flag.StringP("source", "s", "uniform", "Source: uniform|poisson (default: uniform).")
+	var sp = flag.StringP("load-balancer", "l", "modulo", "Load-balaner: modulo|split|roundrobin (default: modulo).")
+	var v = flag.BoolP("verbose", "v", false, "Verbose logging, identical to <-l all:DEBUG>.")
+	flag.Parse()
+	verbose = *v
 
-        log("Creating item list")
-        is := make([]Item, *m)
-        for j := 0; j < *m; j++ {
-                is[j] = IntegerItem{j}
-        }
-        
-        log("Creating source")
-        var s Source
-        switch strings.ToLower(*src) {
-        case "uniform": s = &UniformSource{n: *n, m: *m, i: 0}
-        case "poisson": s = NewPoissonSource(*m, *n, float64(*m)/2.0, &is)
-        default: panic("Unknown source type: " + *src)
-        }
-        
-        log("Creating comm channels")
-        cs := make([]Channel, *k)
-        for j := 0; j < *k; j++ {
-                cs[j] = make(Channel, BufferSize)
-        }
+	log("Creating item list")
+	is := make([]Item, *m)
+	for j := 0; j < *m; j++ {
+		is[j] = IntegerItem{j}
+	}
 
-        log("Creating LB")
-        var lb LoadBalancer
-        switch strings.ToLower(*sp) {
-        case "modulo":     lb = NewModuloLB(*k, cs)
-        case "split":      lb = NewSplitLB(*k, *m, cs)
-        case "roundrobin": lb = NewRoundRobinLB(*k, cs)
-        default: panic("Unknown load-balancer: " + *sp)
-        }
+	log("Creating source")
+	var s Source
+	switch strings.ToLower(*src) {
+	case "uniform":
+		s = &UniformSource{n: *n, m: *m, i: 0}
+	case "poisson":
+		s = NewPoissonSource(*m, *n, float64(*m)/2.0, &is)
+	default:
+		panic("Unknown source type: " + *src)
+	}
 
-        log("Initializing threads")
-        wg := new(sync.WaitGroup)
-        wg.Add(*k)
-        for j := 0; j < *k; j++ {
-                // create data-structure
-                var d DataStructure
-                switch strings.ToLower(*ds) {
-                case "cache":       d = NewLruCache(*m, true)
-                case "statcache":   d = NewLruCache(*m, false)
-                case "mtf":         d = NewMtf()
-                case "linkedlist":  d = NewLinkedList()
-                case "splay":       d = NewSplayTree()
-                case "btree":       d = NewBTree()
-                default: panic("Unknown data structure: " + *ds)
-                }
-                
-                for i := 0; i < *m; i++ {
-                        d.Add(is[i])
-                }
-                // wrap data-structure with a thread
-                log("Thread: adding new thread: %d", j)
-                t := &Thread{id: j, rx: cs[j], d: d}
+	log("Creating comm channels")
+	cs := make([]Channel, *k)
+	for j := 0; j < *k; j++ {
+		cs[j] = make(Channel, BufferSize)
+	}
 
-                // spawn thread
-                go func(t *Thread) {
-                        l := t.d
-                        found := 0
-                        for r := range t.rx {
-                                if verbose {
-                                        log("Thread %d: %s", t.id, l.String())
-                                }
+	log("Creating LB")
+	var lb LoadBalancer
+	switch strings.ToLower(*sp) {
+	case "modulo":
+		lb = NewModuloLB(*k, cs)
+	case "split":
+		lb = NewSplitLB(*k, *m, cs)
+	case "roundrobin":
+		lb = NewRoundRobinLB(*k, cs)
+	default:
+		panic("Unknown load-balancer: " + *sp)
+	}
 
-                                i := l.Find(r)
-                                if i >= 0 {
-                                        found++
-                                }
-                        }
-                        wg.Done()
-                        // fmt.Printf("Thread exited, found: %d\n", found)
-                        log("Thread exited, found: %d\n", found)
-                        return
-                }(t)
-        }
+	log("Initializing threads")
+	ThreadStore = make([]*Thread, *k)
+	wg := new(sync.WaitGroup)
+	wg.Add(*k)
+	for j := 0; j < *k; j++ {
+		// create data-structure
+		var d DataStructure
+		switch strings.ToLower(*ds) {
+		case "cache":
+			d = NewLruCache(*m, true)
+		case "statcache":
+			d = NewLruCache(*m, false)
+		case "mtf":
+			d = NewMtf()
+		case "linkedlist":
+			d = NewLinkedList()
+		case "splay":
+			d = NewSplayTree()
+		case "btree":
+			d = NewBTree()
+		default:
+			panic("Unknown data structure: " + *ds)
+		}
 
-        log("Starting main loop")
-        t0 := time.Now()
-        for {
-                i, err := s.Generate()
-                if err != nil {
-                        break
-                }
-                lb.Assign(i)
-        }
-        // close all channels
-        for j := 0; j < *k; j++ {
-                close(cs[j])
-        }
-        // wait for all threads to finish
-        wg.Wait()
-        t1 := time.Now()
-        d := t1.Sub(t0)
+		// wrap data-structure with a thread
+		t := &Thread{id: j, rx: cs[j], d: d}
+		ThreadStore[j] = t
 
-        log("Done")
-        fmt.Printf("%d\t%d\t%d\t%v\t%f\n", *k, *m, *n, d, float64(*n)/d.Seconds())
+		// spawn thread for initialization
+		go func(t *Thread) {
+			for i := 0; i < *m; i++ {
+				t.d.Add(is[i])
+			}
+			wg.Done()
+		}(t)
+	}
+	wg.Wait()
+
+	log("Initialization done, starting workers")
+	wg.Add(*k)
+	for j := 0; j < *k; j++ {
+		// spawn worker thread
+		go func(t *Thread) {
+			l := t.d
+			found := 0
+			tt0 := time.Now()
+
+			for r := range t.rx {
+				// if verbose {
+				// 	log("Thread %d: %s", t.id, l.String())
+				// }
+				if verbose && found == 0 {
+					tt0 = time.Now() // reset thread timer: this is the real point where thread starts
+					log("Entering thread %d", t.id)
+				}
+
+				i := l.Find(r)
+				if i >= 0 {
+					found++
+				}
+			}
+			wg.Done()
+
+			// fmt.Printf("Thread %d exited, found: %d\n", t.id, found)
+
+			tt1 := time.Now()
+			dt := tt1.Sub(tt0)
+
+			// fmt.Printf("Thread %d exited, found: %d, running time: %s\n", t.id, found, dt)
+
+			log("Thread %d exited, found: %d, running time: %s", t.id, found, dt)
+
+			return
+		}(ThreadStore[j])
+	}
+
+	log("Starting main loop")
+	t0 := time.Now()
+	for {
+		i, err := s.Generate()
+		if err != nil {
+			break
+		}
+		lb.Assign(i)
+	}
+	// close all channels
+	for j := 0; j < *k; j++ {
+		close(cs[j])
+	}
+	// wait for all threads to finish
+	wg.Wait()
+	t1 := time.Now()
+	d := t1.Sub(t0)
+
+	fmt.Printf("%d\t%d\t%d\t%v\t%f\n", *k, *m, *n, d, float64(*n)/d.Seconds())
 }
